5ee154340a0daea47f17c24aee4e596608424e12
yoniys <64745990+yoniys@users.noreply.github.com>
2020-05-31 14:57:30 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+let findImageIndex=document.getElementsByClassName("slider_content")
+console.log(findImageIndex.offsetLeft);
+console.log("fdssssss")
+let findImageIndex1=document.getElementById("indexLest")
+let d=findImageIndex.offsetLeft;
+console.log(d)
